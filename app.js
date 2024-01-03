aa3286afc9062b2a207ecfd61d19ac81dd075f5f
TanmaySDGEEK <91903110+TanmaySDGEEK@users.noreply.github.com>
2024-01-03 19:03:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let input = prompt("What would you like to do");
+const todos = [];
+while (input !== 'quit') {
+    if (input === 'list') {
+        console.log('**********');
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('**********');
+    }
+    else if (input == 'new') {
+        const newTodo = prompt('Ok,what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to list`);
+    }
+    else if (input === 'delete') {
+        const indexStr = prompt('ok,enter an index to delete');
+        const index = parseInt(indexStr);
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok ,deleted ${deleted[0]}`);
+        } else {
+            console.log("Unknown index")
+        }
+    }
+    input = prompt("what would you like to do");
+}
+console.log("Ok Quit the App");
